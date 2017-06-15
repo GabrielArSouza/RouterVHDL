23b57558b4d51a73db5e9436ec76a0d54477d16d
Gabriel Araújo de Souza <gabriel_feg@hotmail.com>
2017-06-15 13:50:25 -0300

    mux e arbitro funcionando

--- arbitro.vhd
+++ arbitro.vhd
@@ -5,15 +5,16 @@ use ieee.std_logic_unsigned.all;
 entity arbitro is 
 	generic ( w: natural := 8);
 
-	port ( input:  in  std_logic_vector (w-1 downto 0); -- entrada
-	       clk:    in  bit; --clock
-	       output: out std_logic_vector (w-1 downto 0)  --saída 
-	      );
+	port ( clk:                in  bit;  -- clock
+	       in1, in2, in3, in4: in  std_logic_vector  (w-1 downto 0); -- entrada de dados
+	       write_b:            in  bit_vector( 1 downto 0 );  -- escrever no buffer 
+	       output:             out std_logic_vector (w-1 downto 0)); --mesma saída do MUX
 end arbitro;
 
 architecture arch_arb of arbitro is
 	type memory is array(0 to 3) of bit;
-	signal buffer_arb : memory; --_ memória
+	signal buffer_arb : memory; -- memória
+	signal ctrl_1, ctrl_2: bit; -- sinal controlador para o mux
 	
 	-- pos = 0 -> Dado da entrada norte espera para sair
 	-- pos = 1 -> Dado da entrada sul espera para sair
@@ -21,41 +22,66 @@ architecture arch_arb of arbitro is
 	-- pos = 3 -> Dado da emtrada oeste espera para sair
 
 	component mux_4x1_Wbits
-
-		port ( 
-		a0, a1, a2, a3 : in bit_vector (w-1 downto 0);  -- data inputs
-	  	s0, s1         : in std_logic;		   	-- seletores
-	   	s              : out bit_vector (w-1 downto 0));-- saida
-
+ 
+		port(
+		a0, a1, a2, a3 : in  std_logic_vector  (w-1 downto 0); -- entrada de dados
+	   	s0, s1         : in  bit;       		       -- seletores
+	   	s              : out std_logic_vector (w-1 downto 0)); -- saida
 	end component;
 
 	-- s0 = 1 and s1 = 0 -> libera a primeira entrada do MUX (a0 norte)
 	-- s1 = 1 and s0 = 0 -> libera a segunda entrada do MUX (a1 sul)
 	-- s0 and s1 = 0 -> libera a terceira entrada do MUX (a2 leste)
 	-- s0 and s1 = 1 -> libera a quarta entrada do MUX (a3 oeste)
-
+	
 begin
 	process(clk)
 	variable ptr_rd: natural;
-	
+		
 	begin
 		if ( clk'EVENT AND clk='1' ) then
-			if ( (ptr_rd = 0) and (buffer_arb(ptr_rd) = '1') ) then
-				s0 <= '1';
-				s1 <= '0';
-			elsif ( (ptr_rd = 1) and (buffer_arb(ptr_rd) = '1')) then
-				s1 <= '1';
-				s0 <= '0';
-			elsif ( (ptr_rd = 2) and (buffer_arb(ptr_rd) = '1')) then
-				s0 <= '0';
-				s1 <= '0';
-			elsif ( (ptr_rd = 3) and (buffer_arb(ptr_rd) = '1')) then
-				s0 <= '1';
-				s1 <= '1';
+			-- escreve no buffer
+
+			-- write_b = 00 -> Escreve na posição 0(norte)
+			-- write_b = 01 -> Escreve na posição 1(sul)
+			-- write_b = 10 -> Escreve na posição 2(leste)
+			-- write_b = 11 -> Escreve na posição 3(oeste)
+	 
+			if ((write_b = "00") and (buffer_arb(0) = '0')) then
+			    buffer_arb(0) <= '1';
+			elsif ((write_b = "01") and (buffer_arb(1) = '0')) then
+			    buffer_arb(1) <= '1';
+			elsif ((write_b = "10") and (buffer_arb(2) = '0')) then
+			    buffer_arb(2) <= '1';
+			elsif ((write_b = "11") and (buffer_arb(3) = '0')) then
+			    buffer_arb(3) <= '1';
 			end if;
-			rd_ptr := (rd_ptr+1) rem 4;
+
+			-- sai o norte
+			if ( (ptr_rd = 0) and (buffer_arb(ptr_rd) = '1') ) then
+				ctrl_1 <= '1';
+				ctrl_2 <= '0';
+				buffer_arb(ptr_rd) <= '0';
+			-- sai o sul
+			elsif ( (ptr_rd = 1) and (buffer_arb(ptr_rd) = '1')) then
+				ctrl_1 <= '0';
+				ctrl_2 <= '1';
+				buffer_arb(ptr_rd) <= '0';
+			-- sai o leste
+			elsif ( (ptr_rd = 2) and (buffer_arb(ptr_rd) = '1')) then
+				ctrl_1 <= '0';
+				ctrl_2 <= '0';
+				buffer_arb(ptr_rd) <= '0';
+			-- sai o oeste
+			elsif ( (ptr_rd = 3) and (buffer_arb(ptr_rd) = '1')) then
+				ctrl_1 <= '1';
+				ctrl_2 <= '1';
+				buffer_arb(ptr_rd) <= '0';
+			end if;
+			ptr_rd := (ptr_rd+1) rem 4;
 		end if;
-
+		
 	end process;
-
+		-- aplicando dados no mux
+		mux1 : mux_4x1_Wbits port map (in1, in2, in3, in4, ctrl_1, ctrl_2, output);
 end arch_arb;
--- mux_8x1.vhd
+++ mux_8x1.vhd
@@ -3,26 +3,35 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
 entity mux_4x1_Wbits is
-    generic ( w : natural := 4);
+    generic ( w : natural := 8);
     
-    port ( a0, a1, a2, a3 : in bit_vector (w-1 downto 0);  -- data inputs
-	   s0, s1         : in std_logic;		   -- seletores
-	   s              : out bit_vector (w-1 downto 0)); -- saida
+    port ( a0, a1, a2, a3 : in  std_logic_vector  (w-1 downto 0); -- entrada de dados
+	   s0, s1         : in  bit;       		          -- seletores
+	   s              : out std_logic_vector (w-1 downto 0));  -- saida
 end mux_4x1_Wbits;
 
 architecture arch_mux of mux_4x1_Wbits is
--- a0 igual (p)
--- a1 maior (p-m) 
--- a2 menor (m-p)
+-- a0 norte
+-- a1 sul 
+-- a2 leste
+-- a3 oeste 
+
 begin
+
+    -- s0 = 1 and s1 = 0 -> libera a primeira entrada do MUX (a0 norte)
+    -- s1 = 1 and s0 = 0 -> libera a segunda entrada do MUX (a1 sul)
+    -- s0 and s1 = 0 -> libera a terceira entrada do MUX (a2 leste)
+    -- s0 and s1 = 1 -> libera a quarta entrada do MUX (a3 oeste)
    PROCESS(s0,s1, a0, a1, a2, a3)
    BEGIN 
-      IF(s0='1') THEN  -- primeiro
+      IF(s0='1' and s1='0') THEN  -- norte
          s <= a0;
-      ELSIF(s1='1') THEN --segundo
+      ELSIF(s0='0' and s1='1') THEN -- sul
   	 s <= a1;
-      ELSE
+      ELSIF(s0='0' and s1='0') THEN -- leste
 	 s <= a2;
+      ELSIF(s0='1' and s1='1') THEN -- oeste
+	 s <= a3;
       END IF;
    END PROCESS; 
 end arch_mux;
